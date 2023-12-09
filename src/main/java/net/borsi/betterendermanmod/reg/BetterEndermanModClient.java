36ed82b4042b592843e186ad24e258c5c9d37542
Lorenzo Colelli <lorenzo.colelli@protonmail.com>
2023-12-09 11:17:11 +0100

    Initial commit

---
+++ src/main/java/net/borsi/betterendermanmod/reg/BetterEndermanModClient.java
@@ -0,0 +1,10 @@
+package net.borsi.betterendermanmod.reg;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterEndermanModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/borsi/betterendermanmod/reg/BetterEndermanModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.borsi.betterendermanmod.reg;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterEndermanModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
